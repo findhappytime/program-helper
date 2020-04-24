b659204efb9672f733cb9f1e9e218b4b8e8c35fa
zhangshu09 <zhangshu09@meituan.com>
2020-04-24 17:16:22 +0800

    增加parent-pom定义规范

---
+++ helper-base/src/main/java/fun/findhappytime/utils/encode/AESUtils.java
@@ -0,0 +1,109 @@
+package fun.findhappytime.utils.encode;
+
+import org.apache.commons.codec.binary.Base64;
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+
+/**
+ * @Description: 对称加密
+ * @Auther: shuzhang
+ * @Date: 2018/11/2
+ * @Version: 1.0
+ */
+public class AESUtils {
+    private static final Logger LOGGER = LoggerFactory.getLogger(AESUtils.class);
+    private static final String KEY_ALGORITHM = "AES";
+    //默认的加密算法
+    private static final String DEFAULT_CIPHER_ALGORITHM = "AES/ECB/PKCS5Padding";
+
+    /**
+     * AES 加密操作
+     *
+     * @param content  待加密内容
+     * @param password 加密密码
+     * @return 返回Base64转码后的加密数据
+     */
+    public static String encrypt(String content, String password) {
+        try {
+            // 创建密码器
+            Cipher cipher = Cipher.getInstance(DEFAULT_CIPHER_ALGORITHM);
+            byte[] byteContent = content.getBytes("utf-8");
+            // 初始化为加密模式的密码器
+            cipher.init(Cipher.ENCRYPT_MODE, getSecretKey(password));
+            // 加密
+            byte[] result = cipher.doFinal(byteContent);
+            //通过Base64转码返回
+            return Base64.encodeBase64String(result);
+        } catch (Exception ex) {
+            LOGGER.error("encrypt error", ex);
+        }
+
+        return null;
+    }
+
+    /**
+     * AES 解密操作
+     *
+     * @param content
+     * @param password
+     * @return
+     */
+    public static String decrypt(String content, String password) {
+        try {
+            //实例化
+            Cipher cipher = Cipher.getInstance(DEFAULT_CIPHER_ALGORITHM);
+            //使用密钥初始化，设置为解密模式
+            cipher.init(Cipher.DECRYPT_MODE, getSecretKey(password));
+            //执行操作
+            byte[] result = cipher.doFinal(Base64.decodeBase64(content));
+
+            return new String(result, "utf-8");
+        } catch (Exception ex) {
+            LOGGER.error("decrypt error", ex);
+
+        }
+
+        return null;
+    }
+
+    /**
+     * 生成加密秘钥
+     *
+     * @return
+     */
+    private static SecretKeySpec getSecretKey(final String password) {
+        if (StringUtils.isBlank(password)) {
+            throw new NullPointerException("password  is null");
+        }
+
+        //返回生成指定算法密钥生成器的 KeyGenerator 对象
+        KeyGenerator kg = null;
+        try {
+            SecureRandom random = SecureRandom.getInstance("SHA1PRNG");
+            random.setSeed(password.getBytes());
+            kg = KeyGenerator.getInstance(KEY_ALGORITHM);
+
+            //AES 要求密钥长度为 128
+            kg.init(128, random);
+
+            //生成一个密钥
+            SecretKey secretKey = kg.generateKey();
+
+            // 转换为AES专用密钥
+            return new SecretKeySpec(secretKey.getEncoded(), KEY_ALGORITHM);
+        } catch (NoSuchAlgorithmException ex) {
+            LOGGER.error("getSecretKey error", ex);
+        }
+
+        return null;
+    }
+
+}
